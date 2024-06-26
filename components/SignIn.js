e72dd020f1c32fbc900100faca438c2a20e8a774
renaudMaupoux <renaud@ateliermaupoux.com>
2024-06-26 21:44:41 +0200

    avancement front

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,15 +1,15 @@
 import styles from '../styles/SignIn.module.css';
 import { useState } from 'react';
-import { useDispatch , useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../reducers/user';
 function SignIn() {
 
   const[signInUsername, setSignInUsername] = useState('');
   const[signInPassword, setSignInPassword] = useState('');
+  const dispatch = useDispatch();
 
 	const handleConnection = () => {
 
-    const dispatch = useDispatch();
 		fetch('http://localhost:3000/users/signin', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
