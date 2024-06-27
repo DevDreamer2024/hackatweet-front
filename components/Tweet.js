c9614407ca211f0e7f3b11fc62441c23747df487
Aurélien <aurelien.catala@outlook.com>
2024-06-27 21:06:59 +0200

    style et like en attente reducer

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,11 +1,15 @@
 import styles from "../styles/Tweet.module.css";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart , faTrash } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 function Tweet() {
   const dispatch = useDispatch();
   const usertoken = useSelector((state) => state.user.token);
   const [tweetslist, setTweetslist] = useState([]);
+  
+
 
   useEffect(() => {
     const fetchTweets = () => {
@@ -34,6 +38,68 @@ function Tweet() {
       );
   };
 
+
+  const dataFormatted = (date) => {
+
+    const tweetDate = new Date(date);
+
+    const now = new Date();
+    const diff = now - tweetDate;
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+
+    if (days > 0) {
+      return `${days} days ago`;
+    } else if (hours > 0) {
+      return `${hours} hours ago`;
+    } else if (minutes > 0) {
+      return `${minutes} minutes ago`;
+    } else {
+      return `${seconds} seconds ago`;
+    }
+  };
+
+
+
+
+  const likeTweet = (tweetId) => {
+    // Optimistic UI update
+    setTweetslist((prevTweets) =>
+      prevTweets.map((tweet) =>
+        tweet._id === tweetId ? { ...tweet, likecount: tweet.likecount + 1 } : tweet
+      )
+    );
+  
+    fetch(`http://localhost:3000/messages/togglelike/${tweetId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userToken: usertoken,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTweetslist((prevTweets) =>
+          prevTweets.map((tweet) =>
+            tweet._id === tweetId ? { ...tweet, ...data } : tweet
+          )
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+        // Rollback UI update if API call fails
+        setTweetslist((prevTweets) =>
+          prevTweets.map((tweet) =>
+            tweet._id === tweetId ? { ...tweet, likecount: tweet.likecount - 1 } : tweet
+          )
+        );
+      });
+  };
+  
   return (
     <div>
       <div className={styles.tweetContainer}>
@@ -47,24 +113,27 @@ function Tweet() {
                 height={70}
               />
               <p>
-                {tweet.userToken.firstname} {tweet.userToken.username}{" "}
+                {tweet.userToken.firstname} {tweet.userToken.username}
                 <span className={styles.date}>
-                  {new Date(tweet.date).toLocaleString()}
+                  {dataFormatted(tweet.date)}
                 </span>
               </p>
             </div>
             <div className={styles.tweetText}>
               <p>
-                {tweet.text}{" "}
+                {tweet.text}
                 <span className={styles.hashtag}>{tweet.hashtag}</span>
               </p>
             </div>
             <div className={styles.tweetFooter}>
+              <FontAwesomeIcon icon={faHeart} onClick={() => likeTweet(tweet._id)}/>
               <p>{tweet.likecount} likes</p>
+              </div>
               {tweet.userToken.token === usertoken && (
-                <button onClick={() => removeTweet(tweet._id)}>Remove</button>
+                <div className={styles.deleteTweet}>
+                <FontAwesomeIcon icon={faTrash} onClick={() => removeTweet(tweet._id)} />
+              </div>
               )}
-            </div>
           </div>
         ))}
       </div>
